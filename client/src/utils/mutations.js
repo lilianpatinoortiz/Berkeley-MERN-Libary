e08d3839f19572c17a96427f69508c3162db5d74
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-11-12 20:43:29 -0800

    Updating: mutations, queries and adding Apollo Client in App.jsx

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,76 @@
+/* 
+
+mutations.js:
+
+LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+
+ADD_USER will execute the addUser mutation.
+
+SAVE_BOOK will execute the saveBook mutation.
+
+REMOVE_BOOK will execute the removeBook mutation.
+
+*/
+
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: savedBook!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId:$bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+`;
